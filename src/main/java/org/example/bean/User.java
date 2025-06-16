2c5095eca34064e55fa6d6769ad8052560e998b2
Marvelbad <badra.aqua@gmail.com>
2025-06-16 18:38:53 +0300

    added UserDaoJDBCImpl class

---
+++ src/main/java/org/example/bean/User.java
@@ -0,0 +1,8 @@
+package org.example.bean;
+
+public class User {
+    private long id;
+    private String name;
+    private String lastName;
+    private byte age;
+}
---
+++ src/main/java/org/example/util/Util.java
@@ -0,0 +1,27 @@
+package org.example.util;
+
+import lombok.experimental.UtilityClass;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Collection;
+
+@UtilityClass
+public class Util {
+    private String url = "jdbc:mysql://localhost:3306/test";
+    private String userName = "root";
+    private String password = "root";
+
+    public Connection getJDBCConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, userName, password);
+        } catch (SQLException e) {
+            System.out.println("Соединение не установлено");
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+}
